237964972647c06023f6ce0da937b2904c996be2
Malcolm St. John <malcolm19@hotmail.com>
2021-11-06 17:28:16 -0400

    missing setup function for adding an achievement and getting it's id

--- pkg/database/mongoDB_test.go
+++ pkg/database/mongoDB_test.go
@@ -2,16 +2,40 @@ package database
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/Ubivius/microservice-achievements/pkg/data"
 	"github.com/google/uuid"
 )
 
+func integrationTestSetup(t *testing.T) {
+	t.Log("Test setup")
+
+	if os.Getenv("DB_USERNAME") == "" {
+		os.Setenv("DB_USERNAME", "admin")
+	}
+	if os.Getenv("DB_PASSWORD") == "" {
+		os.Setenv("DB_PASSWORD", "pass")
+	}
+	if os.Getenv("DB_PORT") == "" {
+		os.Setenv("DB_PORT", "27888")
+	}
+	if os.Getenv("DB_HOSTNAME") == "" {
+		os.Setenv("DB_HOSTNAME", "localhost")
+	}
+
+	err := deleteAllAchievementsFromMongoDB()
+	if err != nil {
+		t.Errorf("Failed to delete existing items from database during setup")
+	}
+}
+
 func TestMongoDBConnectionAndShutdownIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	mp := NewMongoAchievements()
 	if mp == nil {
@@ -24,6 +48,7 @@ func TestMongoDBAddAchievementIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	achievement := &data.Achievement{
 		Name:        "testName",
@@ -37,6 +62,14 @@ func TestMongoDBAddAchievementIntegration(t *testing.T) {
 	if err != nil {
 		t.Errorf("Failed to add achievement to database")
 	}
+
+	achievements := mp.GetAchievements(context.Background())
+	if achievements == nil || len(achievements) != 1 {
+		t.Error("Incorrect number of returned achivements")
+	}
+	if achievements != nil && achievements[0].Name != achievement.Name {
+		t.Errorf("Achievement is not the same. Expected name : %s but got %s", achievement.Name, achievements[0].Name)
+	}
 	mp.CloseDB()
 }
 
@@ -44,6 +77,7 @@ func TestMongoDBUpdateAchievementIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	achievement := &data.Achievement{
 		ID:          uuid.NewString(),
@@ -65,6 +99,7 @@ func TestMongoDBGetAchievementsIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	mp := NewMongoAchievements()
 	achievements := mp.GetAchievements(context.Background())
@@ -79,6 +114,7 @@ func TestMongoDBGetAchievementByIDIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	mp := NewMongoAchievements()
 	_, err := mp.GetAchievementByID(context.Background(), "c9ddfb2f-fc4d-40f3-87c0-f6713024a993")
