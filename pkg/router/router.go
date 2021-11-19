e6328fb4d6ad50efb67b8acc9a2fa86aa9fa6bbe
JeremiSavard <jeremi.savard@usherbrooke.ca>
2021-11-19 16:20:27 -0500

    add auth middleware to router

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Ubivius/microservice-achievements/pkg/handlers"
 	"github.com/Ubivius/pkg-telemetry/metrics"
+	tokenValidation "github.com/Ubivius/shared-authentication/pkg/auth"
 	"github.com/gorilla/mux"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux"
 )
@@ -18,25 +19,30 @@ func New(achievementHandler *handlers.AchievementsHandler) *mux.Router {
 
 	// Get Router
 	getRouter := router.Methods(http.MethodGet).Subrouter()
+	getRouter.Use(tokenValidation.Middleware)
 	getRouter.HandleFunc("/achievements", achievementHandler.GetAchievements)
 	getRouter.HandleFunc("/achievements/{id:[0-9a-z-]+}", achievementHandler.GetAchievementByID)
 
 	//Health Check
-	getRouter.HandleFunc("/health/live", achievementHandler.LivenessCheck)
-	getRouter.HandleFunc("/health/ready", achievementHandler.ReadinessCheck)
+	healthRouter := router.Methods(http.MethodGet).Subrouter()
+	healthRouter.HandleFunc("/health/live", achievementHandler.LivenessCheck)
+	healthRouter.HandleFunc("/health/ready", achievementHandler.ReadinessCheck)
 
 	// Put router
 	putRouter := router.Methods(http.MethodPut).Subrouter()
+	putRouter.Use(tokenValidation.Middleware)
 	putRouter.HandleFunc("/achievements", achievementHandler.UpdateAchievements)
 	putRouter.Use(achievementHandler.MiddlewareAchievementValidation)
 
 	// Post router
 	postRouter := router.Methods(http.MethodPost).Subrouter()
+	postRouter.Use(tokenValidation.Middleware)
 	postRouter.HandleFunc("/achievements", achievementHandler.AddAchievement)
 	postRouter.Use(achievementHandler.MiddlewareAchievementValidation)
 
 	// Delete router
 	deleteRouter := router.Methods(http.MethodDelete).Subrouter()
+	deleteRouter.Use(tokenValidation.Middleware)
 	deleteRouter.HandleFunc("/achievements/{id:[0-9a-z-]+}", achievementHandler.Delete)
 
 	return router
